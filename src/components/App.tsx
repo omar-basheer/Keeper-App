6bde72814032ee14bead431f0a4d287c1ae725c6
omar-basheer <112007297+omar-basheer@users.noreply.github.com>
2023-07-11 17:09:38 +0000

    rollback to before adding addnote functionality

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,90 +1,23 @@
-import React, { useEffect, useState } from "react"
-import Header from "./Header"
-import Footer from "./Footer"
-import Note from "./Note"
-import notes from "../notes"
-import { Note as Notetype} from "./Types" 
-
-// function createNote(note: {key: number,title: string,content: string}){
-//     return(
-//         <Note 
-//         key={note.key}
-//         title={note.title}
-//         content={note.content}
-//         />
-//     )
-// }
-
-
-function addNote(n: Notetype){
-
-    return(
-        <div>
-            <h1>{n.title}</h1>
-            <p>{n.description}</p>
-        </div>
-    )
-}
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
 
 function App() {
-    const[title,setTitle] = useState<string>("")
-    const[description,setDescription] = useState<string>("")
-
-    const [localNote, setLocalNote] = useState<Array<Notetype>>([]);
-    useEffect(() => {
-        setLocalNote(notes);
-    },
-    [])
-
-    function addToLocalNote(m: Notetype ){
-        setLocalNote([...localNote, m])
-    }
-
-    return (
-            <div>
-                <Header />
-                {localNote.map((noteItem, index) => {
-                        return(
-                            <Note 
-                            key={index}
-                            title={noteItem.title}
-                            content={noteItem.description}
-                            />
-                        )
-                })}
-
-                <input onChange={(event) => {
-                    setTitle(event.target.value)
-                }}
-                value = {title}
-                ></input>
-                <textarea onChange={(event) =>{
-                    setDescription(event.target.value)
-                }}
-                value ={description}
-                ></textarea>
-                <button onClick={()=>{
-                    addToLocalNote(
-                        {
-                            title, description, key:localNote.length
-                        }
-                    )
-                    setTitle("")
-                    setDescription("")
-                }}>add to note</button>
-
-                {/* {addNote} */}
-                {/* {notes.map(note =>(
-                    <Note 
-                    key={note.key}
-                    title={note.title}
-                    content={note.content}
-                    />
-                ))} */}
-                <Footer />
-            </div>
-
-    )
+  return (
+    <div>
+      <Header />
+      {notes.map(noteItem => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/notes.tsx
+++ src/notes.tsx
@@ -1,30 +1,33 @@
-import {Note as Notetype} from "./components/Types"
-
-const notes: Array<Notetype> = [
+const notes = [
   {
     key: 1,
     title: "Delegation",
-    description:
+    content:
       "Q. How many programmers does it take to change a light bulb? A. None – It’s a hardware problem"
   },
   {
     key: 2,
     title: "Loops",
-    description:
+    content:
       "How to keep a programmer in the shower forever. Show him the shampoo bottle instructions: Lather. Rinse. Repeat."
   },
   {
     key: 3,
     title: "Arrays",
-    description:
+    content:
       "Q. Why did the programmer quit his job? A. Because he didn't get arrays."
   },
   {
     key: 4,
     title: "Hardware vs. Software",
-    description:
+    content:
       "What's the difference between hardware and software? You can hit your hardware with a hammer, but you can only curse at your software."
+  },
+  {
+    key: 5,
+    title: "Big ideas",
+    content: "Eat more sushi"
   }
 ];
 
-export default notes
+export default notes;
